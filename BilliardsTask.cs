bd4d56b73d0b12fcb20210e9aa8dc8552b6a3e16
Сергей <wimble@Air-Sergej.mshome.net>
2022-06-20 12:29:31 +0500

    commit

---
+++ BilliardsTask.cs
@@ -0,0 +1,16 @@
+﻿namespace Billiards
+{
+    public static class BilliardsTask
+    {
+        /// <summary>
+        /// Метод возвращает угол отскока шарика от стены по углу направления движения шара и углу
+        /// </summary>
+        /// <param name="directionRadians">Угол направления движения шара</param>
+        /// <param name="wallInclinationRadians">Угол</param>
+        /// <returns>Угол отскока шарика от стены</returns>
+        public static double BounceWall(double directionRadians, double wallInclinationRadians)
+        { 
+            return 2 * wallInclinationRadians - directionRadians;
+        }
+    }
+}
